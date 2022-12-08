6c8cf5bdbf805bd7179b5253b9074fa738983a30
Xavibright9 <gidochekus94@gmail.com>
2022-12-08 06:59:50 -0800

    put c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- * /
- int main(void)
-  {
-           puts("\"Programming is like building a multilingual puzzle");
-           return (0);
-  }
+#include <stdio.h>                                                                                                                                                
+/**                                                                                                                                                               
+ *  * main - Entry point                                                                                                                                             
+ *   * Description: prints "\"Programming is like building a multilingual puzzle"                                                                                     
+ *    * Return: 0                                                                                                                                                      
+ *     */                                                                                                                                                               
+int main(void)                                                                                                                                                    
+{                                                                                                                                                                 
+	                puts("\"Programming is like building a multilingual puzzle");                                                                                     
+			                        return (0);                                                                                                                               
+}
