7184b78443f0798637fce4570dd027044b1f7089
Xavibright9 <gidochekus94@gmail.com>
2022-12-08 07:28:50 -0800

    print c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints "\"With proper grammer, but the outcome is a piece of art"
+ *Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
