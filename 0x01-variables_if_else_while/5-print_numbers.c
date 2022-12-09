df07a2d66cae4b703ebe815495140e9723d01c9b
Xavibright9 <gidochekus94@gmail.com>
2022-12-09 14:40:04 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all strings from 0 to 9
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		printf("%d", num);
+
+	printf("\n");
+	return (0);
+}
