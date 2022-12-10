cae2dd5c69beebee4a4396ff6900ea0781a84d1d
Xavibright9 <gidochekus94@gmail.com>
2022-12-10 13:52:20 -0800

    three alpha combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,7 +11,7 @@ int main(void)
 
 	int digit1, digit2, digit3;
 
-	for (digit1 = 0; digit1 < 8; digit1++)
+	for (digit1 = 0; digit1 < 8; digit++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
@@ -19,7 +19,7 @@ int main(void)
 			{
 				putchar((digit1 % 10) + '0');
 				putchar((digit2 % 10) + '0');
-				putchar((digit3 % 10) = '0');
+				putchar((digit3 % 10) + '0');
 
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 					continue;
