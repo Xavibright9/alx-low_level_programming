6b53df7e4121de62c43845ebce9b33b2a2b2f206
Xavibright9 <gidochekus94@gmail.com>
2022-12-10 13:56:55 -0800

    three alpha combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,7 +3,7 @@
 /**
  * main - Prints all possible combinations of three different digits,
  * in ascending order, separated by a comma followed by a space.
- * Return: Always 0 (Success)
+i * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -11,7 +11,7 @@ int main(void)
 
 	int digit1, digit2, digit3;
 
-	for (digit1 = 0; digit1 < 8; digit++)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
