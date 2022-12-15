f5931a68755b1b1571e75e2de608f0e197753496
Xavibright9 <gidochekus94@gmail.com>
2022-12-15 05:54:07 -0800

    Symbols

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n:.input integer parameter
+ *
+ * owned by me
+ */
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+
+	_putchar(i % 10 + '0');
+}
